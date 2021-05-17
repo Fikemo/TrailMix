8bdbfffa0b0d7e1c0554ca823f74f06634946c39
Aubrey_Javachip <aschelba@ucsc.edu>
2021-05-17 16:37:10 -0700

    Implemented Tutorial Scene

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import GameManager from './scenes/GameManager.js';
 import Menu from './scenes/Menu.js';
+import Tutorial from './scenes/Tutorial.js';
 import Load from './scenes/Load.js';
 import TestRight from './scenes/TestRight.js';
 import TestDownLeft from './scenes/TestDownLeft.js';
@@ -10,7 +11,7 @@ import LevelTemplate from './scenes/LevelTemplate.js';
 let config = {
     width: 640,
     height: 640,
-    scene: [Load, Menu, GameManager],
+    scene: [Load, Menu, Tutorial, GameManager],
     physics: {
         default: 'arcade',
         arcade: {
