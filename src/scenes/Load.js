69012002a34937bea50d32f735f313a5de4336ea
elorenz1 <elorenz@ucsc.edu>
2021-06-01 17:56:51 -0700

    enemy injury sound added

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -101,6 +101,7 @@ export default class Load extends Phaser.Scene {
         // sfx
         this.load.audio('sfx_jump', 'JumpSound.wav');
         this.load.audio('sfx_bullet', 'gunShot.wav');
+        this.load.audio('sfx_enemy', 'enemyOof.wav');
     }
 
     create(){
