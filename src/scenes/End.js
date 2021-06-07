d7d92b8deb03041bc7635ac32fbc8317e4eb34d2
Aubrey_Javachip <aschelba@ucsc.edu>
2021-06-07 01:08:59 -0700

    updated ForestDownLeftEasy

--- src/scenes/End.js
+++ src/scenes/End.js
@@ -21,7 +21,7 @@ export default class End extends Phaser.Scene{
             fixedWidth: 0
         }
 
-        this.add.text(this.scale.width/2, this.scale.height/2 + 160, 'Game Over',instructConfig).setOrigin(0.5);
+        this.add.text(this.scale.width/2, this.scale.height/2 + 160, 'You Made It!',instructConfig).setOrigin(0.5);
         this.add.text(this.scale.width/2, this.scale.height/2 + 160, 'R to Restart\nM for Main Menu',instructConfig).setOrigin(0.5);
         this.gameOverActivated = true;
 
