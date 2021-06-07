110acdc6e77ce330a1302d08202607daea592b40
Aubrey_Javachip <aschelba@ucsc.edu>
2021-06-07 14:29:02 -0700

    Mouse Click for Menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -37,26 +37,30 @@ export default class Menu extends Phaser.Scene {
 
         this.bgm = this.sound.add('startMenu_bgm', {volume: 0.1});
         //this.bgm.play();
-        this.add.text(game.config.width/2, game.config.height/2 + 100, 'Press SPACE to Start', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 + 200, 'Press Shift for Credits', menuConfig).setOrigin(0.5);
-        //start scenes
-        this.start = this.add.text(game.config.width/2, game.config.height/2 + 200, 'Press Shift for Credits', menuConfig).setOrigin(0.5).setInteractive();
+        
+        //mouse click to start the game
+        this.start = this.add.text(game.config.width/2, game.config.height/2 + 100, 'Click to Start', menuConfig).setOrigin(0.5).setInteractive();
         this.start.on("pointerdown", () => {
-            if (this.active){
                 this.scene.start('gameManagerScene');
                 this.cursorClick = this.sound.add('sfx_cursorClick', {volume: 0.1});
                 this.cursorClick.play();
-            }
+        }, this);
+        //mouse click for credits
+        this.start = this.add.text(game.config.width/2, game.config.height/2 + 200, 'Click for Credits', menuConfig).setOrigin(0.5).setInteractive();
+        this.start.on("pointerdown", () => {
+                this.scene.start('creditScene');
+                this.cursorClick = this.sound.add('sfx_cursorClick', {volume: 0.1});
+                this.cursorClick.play();
         }, this);
     
-
-        this.cursors.on('down', () => {this.scene.start('gameManagerScene')});
-        this.cursors.shift.on('down', () => {this.scene.start('creditScene')});
+        //test scene changes
+        //this.cursors.on('down', () => {this.scene.start('gameManagerScene')});
+        //this.cursors.shift.on('down', () => {this.scene.start('creditScene')});
         
     }
 
     update(time, delta){
         this.bg.tilePositionX -= 1;
-        this.animation = this.anims.play("player_jump", true);
+        //this.animation = this.anims.play("player_jump", true);
     }
 }
